f4b02b23b87c3839d455c8ec613a829df8f3ede0
larry0442 <1451845265@qq.com>
2021-06-01 23:06:28 +0800

    feat: init

---
+++ count-down-source/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <HomePage />
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HonePage'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
+<style>
+#app {
+  width:100%;
+  height: 100%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ count-down-source/src/components/HonePage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="homepage">
+    <canvas
+      id="canvas"
+      style="display:bolck;margin: 50px auto;"
+    >
+      <!-- 优雅降级 -->
+      当前浏览器不支持canvas,请更换火狐浏览器或者Chrome浏览器重试
+    </canvas>
+  </div>
+</template>
+
+<script>
+import digit from '../utils/digit';
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+
+    }
+  },
+  mounted () {
+    this.init();
+  },
+  methods: {
+    init () {
+      console.log(digit);
+      const canvas = document.getElementById('canvas');
+      const context= canvas.getContext('2d');
+      console.log(context);
+
+    },
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.homepage {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  color: red;
+}
+</style>
